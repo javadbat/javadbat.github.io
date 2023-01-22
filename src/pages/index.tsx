033f093d085e210878f163c2b11545f20651e160
javadbat <javadbat@gmail.com>
2023-01-22 14:26:46 +0330

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import { prefix } from '../utils/prefix';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -28,7 +28,7 @@ export default function Home() {
             >
               By{' '}
               <Image
-                src="/vercel.svg"
+                src={`${prefix}/vercel.svg`}
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
@@ -42,7 +42,7 @@ export default function Home() {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src="/next.svg"
+            src={`${prefix}/next.svg`}
             alt="Next.js Logo"
             width={180}
             height={37}
@@ -50,7 +50,7 @@ export default function Home() {
           />
           <div className={styles.thirteen}>
             <Image
-              src="/thirteen.svg"
+              src={`${prefix}/thirteen.svg`}
               alt="13"
               width={40}
               height={31}
