5dd1fb39a57f7807005d15f17727f230af8fd8a6
javadbat <javadbat@gmail.com>
2023-02-17 21:56:27 +0330

    make it responsive

--- src/components/index-page/intro-section/styles.ts
+++ src/components/index-page/intro-section/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {down} from 'styled-breakpoints';
 export const IntroWrapper = styled.section`
   width: 100%;
   height: 100vh;
@@ -9,14 +10,14 @@ export const IntroWrapper = styled.section`
 `
 
 export const PageTitle = styled.h1`
-  font-size: 9.6rem;
+
   text-align: center;
   font-family: "title-font";
   color:var(--p-color);
   transition: all 0.2s 0s ease;
   margin: 0;
   padding: 0;
-  
+
   &:hover{
       color:var(--p-color-light);
     }
@@ -26,13 +27,24 @@ export const Name = styled.span`
     display: block;
     letter-spacing: 0.1rem;
     word-spacing: 0.3rem;
+    ${down("lg")}{
+      font-size:1.4rem;
+      word-spacing: 0.2rem;
+    }
     `
 export const Family = styled.span`
     display: block;
     letter-spacing: -0.5rem;
+    font-size: 9.6rem;
+    ${down("lg")}{
+      font-size:6rem;
+    }
 `;
 export const Subtitle = styled.h2`
     font-size:1.5rem;
     font-weight: 400;
     color:var(--p-color-light);
+    ${down("lg")}{
+      font-size:1rem;
+    }
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>mohammad javad bathaei</title>
         <meta name="description" content="seyed mohammad javad bathaei official website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
