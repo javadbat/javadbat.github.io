b23cc0c5b0a5074e9739927c53f04641988e07c7
javadbat <javadbat@gmail.com>
2023-02-17 09:35:01 +0330

    add styled componnt and show my name

---
+++ src/components/index-page/intro-section/IntroSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PageTitle } from './styles'
+
+function IntroSection() {
+  return (
+    <PageTitle>Seyed Mohammad Javad Bathaei</PageTitle>
+  )
+}
+
+export default IntroSection
