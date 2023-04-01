ee1f382231746e511a44c8e0d90135fe47a49506
javadbat <javadbat@gmail.com>
2023-04-02 02:54:27 +0430

    add props type to exprerience component

--- src/components/index-page/experiences-section/Experience.tsx
+++ src/components/index-page/experiences-section/Experience.tsx
@@ -1,8 +1,13 @@
-import Image from 'next/image';
 import React, { useMemo } from 'react';
 import {CompanyName, ExperienceDate, ExperienceWrapper } from './styles';
 
-function Experience({name,fromDate,ToDate,Logo}) {
+type ExperienceProps = {
+  name:string,
+  fromDate:string,
+  ToDate:string,
+  Logo:React.FC,
+}
+const Experience: React.FC<ExperienceProps> = ({name,fromDate,ToDate,Logo})=>{
     const FromDateString = useMemo(()=>{return Intl.DateTimeFormat("en",{dateStyle:"medium"}).format(new Date(fromDate)) },[fromDate])
     const ToDateString = useMemo(()=>{return Intl.DateTimeFormat("en",{dateStyle:"medium"}).format(new Date(ToDate)) },[ToDate]);
   return (
