7a5fc0c70c1c6bdb2eebfe5e81ac2b73f3b53078
javadbat <javadbat@gmail.com>
2024-06-28 15:52:07 +0330

    change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+ 
+  // Optional: Change the output directory `out` -> `dist`
+   distDir: 'dist',
   compiler:{
     styledComponents:true
   },
